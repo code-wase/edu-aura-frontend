615605d50a835910f8eb49552ce91d570a182c4a
Abdul Mutalib <mutalib92847@gmail.com>
2026-02-05 20:27:15 +0530

    Reupdated the cource and faculty pages

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,20 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:5000/api',
   baseURL: 'https://edu-aura-backend-t1du.vercel.app/api',
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // },
 });
 
-// JSON only when body is JSON
 api.interceptors.request.use((config) => {
-  if (!(config.data instanceof FormData)) {
+  if (config.method !== 'get' && !(config.data instanceof FormData)) {
     config.headers['Content-Type'] = 'application/json';
-  } else {
-    delete config.headers['Content-Type']; // VERY IMPORTANT
   }
+
+  if (config.data instanceof FormData) {
+    delete config.headers['Content-Type'];
+  }
+
   return config;
 });
 
